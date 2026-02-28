36b6e46fa585c8349198509e5ce6e98f511dfac4
Lamgok <andosiahaan13@gmail.com>
2026-02-28 23:13:32 +0700

    update

--- src/main/kotlin/helpers/DatabaseHelper.kt
+++ src/main/kotlin/helpers/DatabaseHelper.kt
@@ -4,11 +4,26 @@ import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
 
 fun Application.configureDatabases() {
-    val dbHost = environment.config.property("ktor.database.host").getString()
-    val dbPort = environment.config.property("ktor.database.port").getString()
-    val dbName = environment.config.property("ktor.database.name").getString()
-    val dbUser = environment.config.property("ktor.database.user").getString()
-    val dbPassword = environment.config.property("ktor.database.password").getString()
+    fun configValue(path: String, fallback: String): String {
+        val key = when (path) {
+            "ktor.database.host" -> "DB_HOST"
+            "ktor.database.port" -> "DB_PORT"
+            "ktor.database.name" -> "DB_NAME"
+            "ktor.database.user" -> "DB_USER"
+            "ktor.database.password" -> "DB_PASSWORD"
+            else -> path.substringAfterLast('.').uppercase()
+        }
+
+        return environment.config.propertyOrNull(path)?.getString()
+            ?: System.getProperty(key)
+            ?: fallback
+    }
+
+    val dbHost = configValue("ktor.database.host", "127.0.0.1")
+    val dbPort = configValue("ktor.database.port", "5432")
+    val dbName = configValue("ktor.database.name", "db_pam_sukus")
+    val dbUser = configValue("ktor.database.user", "postgres")
+    val dbPassword = configValue("ktor.database.password", "postgres")
 
     Database.connect(
         url = "jdbc:postgresql://$dbHost:$dbPort/$dbName",
