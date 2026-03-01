1601d0facfa893a4c83c5f329c6b56c372397ebf
Lamgok <andosiahaan13@gmail.com>
2026-03-01 15:27:15 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -8,3 +8,13 @@ CREATE TABLE IF NOT EXISTS plants (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS sukus (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nama VARCHAR(100) NOT NULL,
+    path_gambar VARCHAR(255) NOT NULL,
+    makanan TEXT NOT NULL,
+    rumahadat TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+    );
